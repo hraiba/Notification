09d6d46d761407a159711953bad8f3d6198f2b14
Mohammad Hraiba <hraibam@gmail.com>
2026-08-29 00:34:54 +0200

    Add validation for notification request payload

--- src/NotificationForwarder.Api/Endpoints/NotificationEndpoints.cs
+++ src/NotificationForwarder.Api/Endpoints/NotificationEndpoints.cs
@@ -17,6 +17,13 @@ public static class NotificationEndpoints
         NotificationProcessor Processor,
         CancellationToken cancellationToken)
     {
+        // Validate the request payload
+        // we can user some validation library like FluentValidation for more complex scenarios
+        // but for this simple case, we will just check if the required fields are not null or empty
+        if (!IsValidPayload(request))
+        {
+            return Results.BadRequest(new { Error = "Invalid payload. Ensure Title, Message, and Level are provided and valid." });
+        }
         try
         {
             var result  = await Processor.Process(request, cancellationToken);
@@ -49,5 +56,16 @@ public static class NotificationEndpoints
         {
             return Results.Problem($"An error occurred while processing the notification: {ex.Message}", statusCode: StatusCodes.Status500InternalServerError);
         }
+    }
+
+    private static bool IsValidPayload(NotificationRequest request)
+    {
+        return !string.IsNullOrWhiteSpace(request.Title) &&
+               !string.IsNullOrWhiteSpace(request.Message) &&
+               IsValidLevel(request.Level);
+    }
+    private static bool IsValidLevel(string level)
+    {
+        return level is "info" or "warning" or "error";
     }
 }
